049808a0ed6cb9f2be1fa240fbda7c2169c5eebb
Buievska <anna111239@gmail.com>
2025-07-27 17:09:50 +0000

    Deploying to gh-pages from @ nataliyarosenberg/internet-store-blended@112da6996011bdfd1ade4b79bf19c6e0dd46ab2c 🚀

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-import"./assets/styles-JE8YjOlG.js";import{a as s}from"./assets/vendor-N5iQpiFS.js";const r="https://dummyjson.com/products",c="/category-list";async function n(){try{return(await s(`${r}${c}`)).data}catch(t){console.error(t)}}const a={categotiesList:document.querySelector(".categories"),productsList:document.querySelector(".products")};function i(t){const e=t.map(o=>`<li class="categories__item">
-   <button class="categories__btn" type="button">${o}</button>
- </li>`).join("");a.categotiesList.insertAdjacentHTML("beforeend",e)}async function u(){const t=await n();i(t)}u();
+import"./assets/styles-JE8YjOlG.js";import{a as e}from"./assets/vendor-N5iQpiFS.js";const o="https://dummyjson.com/products",d="/category-list";async function l(){try{return(await e(`${o}${d}`)).data}catch(t){console.error(t)}}async function m(t=1){try{return(await e(`${o}?limit=12&skip=${(t-1)*12}`)).data}catch(s){console.log(s.message)}}const c={categotiesList:document.querySelector(".categories"),productsList:document.querySelector(".products")};function _(t){const s=t.map(r=>`<li class="categories__item">
+   <button class="categories__btn" type="button">${r}</button>
+ </li>`).join("");c.categotiesList.insertAdjacentHTML("beforeend",s)}function g(t){const s=t.map(({thumbnail:r,id:a,title:n,brand:i,category:p,price:u})=>`
+    <li class="products__item" data-id="${a}">
+    <img class="products__image" src="${r}" alt=""/>
+    <p class="products__title">${n}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand:${i}</span></p>
+    <p class="products__category">Category:${p} </p>
+    <p class="products__price">Price:${u} $</p>
+ </li>`).join("");c.productsList.insertAdjacentHTML("beforeend",s)}async function y(){const t=await l();_(t);const s=await m();g(s.products)}y();
 //# sourceMappingURL=index.js.map
